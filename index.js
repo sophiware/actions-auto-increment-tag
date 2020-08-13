c5ae8f73f5242592209b512ef190970272b885bd
Philippe Assis <codephilippe@gmail.com>
2020-08-13 02:15:28 -0300

    'actions'

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 function increment (tag) {
-  if (tag) {
-    const tagSplit = tag.split('.')
+  const tagSplit = tag.split('.')
 
-    if (tagSplit.length > 1) {
-      return [...tagSplit.slice(0, -1), parseInt(tagSplit[tagSplit.length - 1]) + 1].join('.')
-    }
-
-    return parseInt(tagSplit[0]) + 1
+  if (tagSplit.length > 1) {
+    return [...tagSplit.slice(0, -1), parseInt(tagSplit[tagSplit.length - 1]) + 1].join('.')
   }
 
-  return '0.0.1'
+  return parseInt(tagSplit[0]) + 1
 }
 
 (async () => {
@@ -20,9 +16,10 @@ function increment (tag) {
     const { owner, repo } = github.context.repo
     const token = core.getInput('token')
     const octokit = github.getOctokit(token)
-    const tags = await octokit.repos.listTags({ owner, repo })
-    console.log(tags)
-    core.setOutput('tag', '0')
+    const {data} = await octokit.repos.listTags({ owner, repo })
+    const newTag = (data && data[0]) ? increment(data[0].name) ? '0.0.1'
+        console.log(newTag)
+    core.setOutput('tag', newTag)
   } catch (error) {
     core.setFailed(error.message)
   }
