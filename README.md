79fa751a7fc0acd0763e7c886cfc44ac6b6516d2
Philippe Assis <codephilippe@gmail.com>
2020-08-13 01:06:46 -0300

    'done'

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hello world javascript action
+
+This action adds one more to the last number of a tag. Example: 1.23.1 to 1.23.2, 4.3 to 4.4, 5 to 6 ...
+
+## Outputs
+
+### `tag`
+
+Tag created.
+
+## Exemplo de uso
+
+uses: actions/hello-world-javascript-action@v1
--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require('@actions/core')
+const github = require('@actions/github')
+const { execSync: exec } = require('child_process')
+
+function increment (tag) {
+  if (tag) {
+    const tagSplit = tag.split('.')
+
+    if (tagSplit.length > 1) {
+      return [...tagSplit.slice(0, -1), parseInt(tagSplit[tagSplit.length - 1]) + 1].join('.')
+    }
+
+    return parseInt(tagSplit[0]) + 1
+  }
+
+  return '0.0.1'
+}
 
 (async () => {
   try {
-    const token = core.getInput('token');
-    const octokit = github.getOctokit(token)
-    const lastTag = await octokit.git.getTag()
-    console.log(lastTag)
+    const lastTag = exec('git describe HEAD').toString().trim()
+    const newTag = increment(lastTag)
+    exec(`git tag -a ${newTag} -m "Auto increment to ${newTag}"`)
+    exec('git push --tags')
+    core.setOutput('tag', newTag)
+    console.log(`New tag created: ${newTag}`)
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 })()
