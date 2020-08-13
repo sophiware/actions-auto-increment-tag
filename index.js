d2395e73fce07de616b43a9071a5f0f75dcbf74f
Philippe Assis <codephilippe@gmail.com>
2020-08-13 03:01:08 -0300

    'actions'

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const github = require('@actions/github');
 (async () => {
   try {
     const { owner, repo } = github.context.repo
+    console.log(github)
     const token = core.getInput('token')
     const octokit = github.getOctokit(token)
     const { data } = await octokit.repos.listTags({ owner, repo })
     const tag = (data && data[0])
       ? data[0].name.replace(/(\d+)(?!.*\d)/g, parseInt(data[0].name.match(/(\d+)(?!.*\d)/)[0]) + 1)
       : '0.0.1'
-console.log(github)
     // await octokit.git.createTag({
     //   owner,
     //   repo,
